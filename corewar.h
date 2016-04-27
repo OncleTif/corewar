3a9c4a9bb7f39120f34ac6c76dfc59cd6fcb5fe3
Your Name <you@example.com>
2016-04-27 18:00:27 +0200

    .h2

--- corewar.h
+++ corewar.h
@@ -6,7 +6,7 @@
 /*   By: djoly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/27 17:03:13 by djoly             #+#    #+#             */
-/*   Updated: 2016/04/27 17:48:28 by djoly            ###   ########.fr       */
+/*   Updated: 2016/04/27 17:56:27 by djoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,17 @@ struct s_base_process
 	t_list_process 	**beg_list; // liste des processs
 };
 
+typedef struct s_cpu	t_cpu;
+struct s_cpu
+{
+	int		ir; //instruction registre
+	int		current_cycle; // cycle en cours
+
+};
+
 typedef struct s_base_core	t_base_core;
 struct s_base_core
 {
 	int		nb_joueur;
-	int		current_cycle; // cycle en cours
 	char	core[MEM_SIZE];
 };
