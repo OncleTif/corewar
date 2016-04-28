1d260b87aecc919292493614e269d23dc912ad32
tmanet <tmanet@student.42.fr>
2016-04-28 12:40:45 +0200

    norme sur print magic

--- sources/ft_print_magic.c
+++ sources/ft_print_magic.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 10:21:35 by tmanet            #+#    #+#             */
-/*   Updated: 2016/04/28 11:55:27 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/04/28 12:40:12 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	ft_print_magic(int fd)
 {
 	char	magic[4];
 
-	magic[0] = COREWAR_EXEC_MAGIC /256 /256 /256 % 256;
-	magic[1] = COREWAR_EXEC_MAGIC /256 /256 % 256;
-	magic[2] = COREWAR_EXEC_MAGIC /256 % 256;
+	magic[0] = COREWAR_EXEC_MAGIC / 256 / 256 / 256 % 256;
+	magic[1] = COREWAR_EXEC_MAGIC / 256 / 256 % 256;
+	magic[2] = COREWAR_EXEC_MAGIC / 256 % 256;
 	magic[3] = COREWAR_EXEC_MAGIC % 256;
 	write(fd, magic, sizeof(COREWAR_EXEC_MAGIC));
 }
