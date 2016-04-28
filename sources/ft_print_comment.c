d5c1a6a2fd78759c547820dfd7e2123d53b37ea1
tmanet <tmanet@student.42.fr>
2016-04-28 12:02:35 +0200

    separateurs dans name et comment

--- sources/ft_print_comment.c
+++ sources/ft_print_comment.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 10:52:54 by tmanet            #+#    #+#             */
-/*   Updated: 2016/04/28 10:52:58 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/04/28 12:01:20 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 void	ft_print_comment(t_champ *chp, int fd)
 {
+	char	*zero;
+
+	zero = "\0\0\0\0";
 	write(fd, &chp->comment, COMMENT_LENGTH);
+	write(fd, zero, 4);
 }
--- sources/ft_print_name.c
+++ sources/ft_print_name.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 10:49:13 by tmanet            #+#    #+#             */
-/*   Updated: 2016/04/28 11:22:14 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/04/28 12:02:03 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 void	ft_print_name(t_champ *chp, int fd)
 {
+	char	*zero;
+
+	zero = "\0\0\0\0";
 	write(fd, &chp->name, PROG_NAME_LENGTH);
+	write(fd, zero, 4);
 }
