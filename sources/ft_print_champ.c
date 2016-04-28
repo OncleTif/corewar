a647a4ce0ac6c3a1c76b95f36ddfddd3d53157d7
tmanet <tmanet@student.42.fr>
2016-04-28 11:34:30 +0200

    print presque ok, changer MSB sur magic

--- sources/ft_print_champ.c
+++ sources/ft_print_champ.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 09:50:09 by tmanet            #+#    #+#             */
-/*   Updated: 2016/04/28 11:01:51 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/04/28 11:33:11 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_print_champ(t_champ *chp)
 {
 	int	fd;
 
-	if ((fd = open(chp->file_n, O_WRONLY)) < 0)
+	if ((fd = open(chp->file_n, O_RDWR | O_CREAT | O_TRUNC, 777)) < 0)
 		ft_error(ft_strjoin("error while opening file ", chp->file_n));
 	ft_print_magic(fd);
 	ft_print_name(chp, fd);
