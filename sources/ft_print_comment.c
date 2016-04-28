60026745b370352d5f91fd0492c1b43799d0ba7e
tmanet <tmanet@student.42.fr>
2016-04-28 11:02:07 +0200

    ajout print

--- sources/ft_print_comment.c
+++ sources/ft_print_comment.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_champ.c                                   :+:      :+:    :+:   */
+/*   ft_print_comment.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/04/28 09:50:09 by tmanet            #+#    #+#             */
-/*   Updated: 2016/04/28 11:01:51 by tmanet           ###   ########.fr       */
+/*   Created: 2016/04/28 10:52:54 by tmanet            #+#    #+#             */
+/*   Updated: 2016/04/28 10:52:58 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/asm.h"
 
-void	ft_print_champ(t_champ *chp)
+void	ft_print_comment(t_champ *chp, int fd)
 {
-	int	fd;
-
-	if ((fd = open(chp->file_n, O_WRONLY)) < 0)
-		ft_error(ft_strjoin("error while opening file ", chp->file_n));
-	ft_print_magic(fd);
-	ft_print_name(chp, fd);
-	ft_print_bin_size(chp, fd);
-	ft_print_comment(chp, fd);
-	ft_print_bin(chp, fd);
-	ft_putendl(ft_strjoin("Writing output program to ", chp->file_n));
-	close(fd);
+	write(fd, &chp->comment, COMMENT_LENGTH);
 }
--- sources/ft_print_magic.c
+++ sources/ft_print_magic.c
@@ -1,28 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_champ.c                                   :+:      :+:    :+:   */
+/*   ft_print_magic.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/04/28 09:50:09 by tmanet            #+#    #+#             */
-/*   Updated: 2016/04/28 11:01:51 by tmanet           ###   ########.fr       */
+/*   Created: 2016/04/28 10:21:35 by tmanet            #+#    #+#             */
+/*   Updated: 2016/04/28 10:48:24 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/asm.h"
 
-void	ft_print_champ(t_champ *chp)
+void	ft_print_magic(int fd)
 {
-	int	fd;
+	int	i;
 
-	if ((fd = open(chp->file_n, O_WRONLY)) < 0)
-		ft_error(ft_strjoin("error while opening file ", chp->file_n));
-	ft_print_magic(fd);
-	ft_print_name(chp, fd);
-	ft_print_bin_size(chp, fd);
-	ft_print_comment(chp, fd);
-	ft_print_bin(chp, fd);
-	ft_putendl(ft_strjoin("Writing output program to ", chp->file_n));
-	close(fd);
+	i = COREWAR_EXEC_MAGIC;
+	write(fd, &i, sizeof(COREWAR_EXEC_MAGIC));
 }
