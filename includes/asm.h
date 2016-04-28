accc36dc032ca303e04af3672dbef2bcc1b026b2
Simon SICARD <ssicard@e2r9p1.42.fr>
2016-04-28 09:03:46 +0200

    envoi de l asc.h modifie

--- includes/asm.h
+++ includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/27 20:08:32 by tmanet            #+#    #+#             */
-/*   Updated: 2016/04/27 20:19:43 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/04/28 09:03:05 by ssicard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,10 @@ struct						s_label
 
 struct						s_champ
 {
+	char					*file_n;
 	char					name[PROG_NAME_LENGTH];
 	char					comment[COMMENT_LENGTH];
+	char					bin[CHAMP_MAX_SIZE];
 	int						pos;
 	t_list					*labels;
 	t_list					*miss;
