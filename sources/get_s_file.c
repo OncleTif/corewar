3448ef82e143b32d71fdfed2956ea356a35c5ffd
Your Name <you@example.com>
2016-04-28 14:45:07 +0200

    s_file.c

--- sources/get_s_file.c
+++ sources/get_s_file.c
@@ -3,28 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   get_s_file.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ssicard <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 09:07:57 by ssicard           #+#    #+#             */
-/*   Updated: 2016/04/28 10:01:19 by ssicard          ###   ########.fr       */
+/*   Updated: 2016/04/28 14:44:19 by djoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/asm.h"
 
-static	void	get_name(char *line)
+static	void	get_str(t_champ *c, char *tmp, char *str)
 {
+	char	*line;
+	int		indic;
+	int		i;
 
-
-
-
+	i = 0;
+	indic = 0;
+	tmp = ft_strchr(tmp, '"');
+		tmp++;
+	while (*tmp)
+	{
+		if (*tmp == '"')
+		{
+			indic = 1;
+			break ;
+		}
+		str[i++] = *tmp++;
+	}
+	while (!indic)
+	{
+		get_next_line(c->fd, &line);
+		str[i++] = '\n';
+		while(*line)
+		{
+			if (*line == '"')
+				indic = 2;
+			else
+				str[i++] = *line;
+			line++;
+		}
+	}
+	str[i] = '\0';
 }
 
 void		read_s_file(t_champ *c, char *file)
 {
 	char	*line;
+	char	*tmp;
 
-	if (!(c->fd = open(file, O_RDONLY)))
+	if ((c->fd = open(file, O_RDONLY)) == -1)
 	{
 		printf("Open failed.\n");
 		return ;
@@ -35,11 +63,12 @@ void		read_s_file(t_champ *c, char *file)
 		{
 			tmp = ft_strtrim(line);
 			if (ft_strncmp(".name", tmp, 5) == 0)
-				get_name(tmp);
+				get_str(c, tmp, c->name);
 			else if (ft_strncmp(".comment", tmp, 8) == 0)
-				get_comment(tmp);
-			free(tmp);
+				get_str(c, tmp, c->comment);
 			// suite lecture
 		}
 	}
+	printf("C_NAME = -->%s<--\n", c->name);
+	printf("C_COMMENT = -->%s<--\n", c->comment);
 }
