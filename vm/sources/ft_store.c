5e7bf4ab6db4fb7a7ae23d0c823fd82e9f628854
tmanet <tmanet@student.42.fr>
2016-05-06 09:52:07 +0200

    st & sti

--- vm/sources/ft_store.c
+++ vm/sources/ft_store.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/05 16:19:10 by tmanet            #+#    #+#             */
-/*   Updated: 2016/05/05 18:06:48 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/05/06 09:51:44 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_st(t_process *proc, t_vm *vm)
 {
 	if (proc->ir.code_args[1] == T_IND)
-		put_mem(vm, proc->pc, proc->ir.args[1],
+		put_mem(vm, proc->pc + proc->ir.args[1],
 				proc->reg[proc->ir.args[0] - 1]);
 	else
 		proc->reg[proc->ir.args[1] - 1] = proc->reg[proc->ir.args[0] - 1];
@@ -28,18 +28,17 @@ void	ft_sti(t_process *proc, t_vm *vm)
 	int	off;
 	int	mod;
 
-	if (proc->ir.code_args[0] == T_IND)
-		off = get_mem(vm, proc->pc, proc->ir.args[0]);
-	else if (proc->ir.code_args[0] == T_REG)
-		off = proc->reg[proc->ir.args[0] - 1];
+	if (proc->ir.code_args[1] == T_IND)
+		off = get_mem(vm, proc->pc + proc->ir.args[1]);
+	else if (proc->ir.code_args[1] == T_REG)
+		off = proc->reg[proc->ir.args[1] - 1];
 	else
-		off = proc->ir.args[0];
-	if (proc->ir.code_args[1] == T_REG)
-		mod = proc->reg[proc->ir.args[1] - 1];
+		off = proc->ir.args[1];
+	if (proc->ir.code_args[2] == T_REG)
+		mod = proc->reg[proc->ir.args[2] - 1];
 	else
-		mod = proc->ir.args[1];
-
-	val = get_mem(vm, proc->pc, off + mod);
-	proc->reg[proc->ir.args[1] - 1] = val;
-	proc->carry = !val;
+		mod = proc->ir.args[2];
+	put_mem(vm, proc->pc + off + mod,
+			proc->reg[proc->ir.args[0] - 1]);
+	proc->carry = !proc->reg[proc->ir.args[0] - 1];
 }
