8c69a745b1c18d47906310ee570f6de62094836a
tmanet <tmanet@student.42.fr>
2016-05-09 16:18:36 +0200

    live ok

--- vm/sources/ft_live.c
+++ vm/sources/ft_live.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/09 12:37:24 by tmanet            #+#    #+#             */
-/*   Updated: 2016/05/09 12:38:17 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/05/09 16:18:18 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 void	ft_live(t_vm *vm, t_process *proc)
 {
-vm++;
-proc++;
+	t_list_player	*plr;
 
+	plr = vm->bplr.lst_plyr;
+	while (plr && plr->plr->num_plyr != proc->ir.args[0])
+		plr = plr->next;
+	if (plr)
+		plr->plr->last_live = vm->cpu.cur_cycle;
+	vm->nbr_live++;
+	proc->live++;
 }
