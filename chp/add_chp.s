d724aba4792b94a820ed1d3684f1461aad298b87
Simon SICARD <ssicard@e1r5p1.42.fr>
2016-05-10 15:29:18 +0200

    envoi chameau simon

---
+++ chp/add_chp.s
@@ -0,0 +1,18 @@
+.name "add_chp"
+.comment "ADD TEST MOTHER FUCKER"
+
+adopi:	ld		256, r1
+    	add		r1, r1, r1
+    	add		r1, r1, r3
+    	add		r1, r3, r2
+
+adopipi:ld		2049, r1
+    	add		r1, r1, r1
+    	add		r1, r1, r1
+    	add		r1, r2, r3
+
+adocaca:ld		4095, r1
+    	add		r1, r1, r1
+    	add		r1, r1, r1
+    	add		r1, r1, r3
+
