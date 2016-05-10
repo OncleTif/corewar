d9388bf2d81d4289bf5ff38ab95117c561a945f0
tmanet <tmanet@student.42.fr>
2016-05-10 16:32:23 +0200

    correction de put_mem

--- vm/sources/put_mem.c
+++ vm/sources/put_mem.c
@@ -6,7 +6,7 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/05 17:49:26 by tmanet            #+#    #+#             */
-/*   Updated: 2016/05/10 14:38:40 by djoly            ###   ########.fr       */
+/*   Updated: 2016/05/10 16:32:00 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ void	put_mem(t_vm *vm, int idx, int off, int val)
 {
 	union u_4o	tmp;
 	int			i;
+	int			j;
 
 	tmp.i = val;
 	i = 0;
+	j = ((idx + (off + IDX_MOD) % (IDX_MOD * 2)) - IDX_MOD) + MEM_SIZE;
 	while (i < 4)
 	{
-		vm->core[(idx + ((off) + i) % IDX_MOD) % MEM_SIZE] = tmp.c[3 - i];
+		vm->core[(j + i) % MEM_SIZE] = tmp.c[3 - i];
 		i++;
 	}
 }
