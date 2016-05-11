a4b764b82669e2dbda6b30fd179760d45b908088
tmanet <tmanet@student.42.fr>
2016-05-11 15:01:59 +0200

    passage du proc dans put_mem

--- vm/sources/put_mem.c
+++ vm/sources/put_mem.c
@@ -6,13 +6,13 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/05 17:49:26 by tmanet            #+#    #+#             */
-/*   Updated: 2016/05/11 14:50:03 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/05/11 15:01:01 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/corewar.h"
 
-void	put_mem(t_vm *vm, int idx, int off, int val)
+void	put_mem(t_vm *vm, t_process *proc, int off, int val)
 {
 	union u_4o	tmp;
 	int			i;
@@ -21,7 +21,7 @@ void	put_mem(t_vm *vm, int idx, int off, int val)
 	tmp.i = val;
 	i = 0;
 	//k = ((idx + (off + IDX_MOD) % (IDX_MOD * 2)) - IDX_MOD) + MEM_SIZE;
-	k = idx + (off % IDX_MOD ) + MEM_SIZE;
+	k = proc->pc + (off % IDX_MOD ) + MEM_SIZE;
 	ft_putnbrendl(k);
 	while (i < 4)
 	{
@@ -30,7 +30,7 @@ void	put_mem(t_vm *vm, int idx, int off, int val)
 	}
 }
 
-void	put_memlong(t_vm *vm, int idx, int val)
+void	put_memlong(t_vm *vm, t_process *proc, int val)
 {
 	union u_4o	tmp;
 	int			i;
@@ -39,7 +39,7 @@ void	put_memlong(t_vm *vm, int idx, int val)
 	i = 0;
 	while (i < 4)
 	{
-		vm->core[(idx + i) % MEM_SIZE] = tmp.c[3 - i];
+		vm->core[(proc->pc + i) % MEM_SIZE] = tmp.c[3 - i];
 		i++;
 	}
 }
