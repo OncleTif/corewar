de5c25bad268a00871f5b2166ca87785378617c4
Simon SICARD <ssicard@e2r9p1.42.fr>
2016-05-12 11:07:29 +0200

    envoi affichage modifie ds fetch next

--- vm/sources/ft_fetch_next.c
+++ vm/sources/ft_fetch_next.c
@@ -6,7 +6,7 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/06 16:20:06 by tmanet            #+#    #+#             */
-/*   Updated: 2016/05/12 09:55:09 by djoly            ###   ########.fr       */
+/*   Updated: 2016/05/12 11:06:54 by ssicard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,17 @@ void	ft_fetch_next(t_vm *vm, t_process *proc)
 	if (proc->pcdelta)
 	{
 		ft_proc_init(vm, proc);
-}
+	}
 	else if (proc->cycle_to_wait != -1)
 	{
-
+		vm->data[proc->pc].pc--;
 		proc->pc = (proc->pc + 1) % MEM_SIZE;
-}
+		vm->data[proc->pc].pc++;
+	}
 	else
 	{
 		proc->cycle_to_wait = 0;
-}
+	}
 	if (vm->core[proc->pc] > 0 && vm->core[proc->pc] <= 16)
 	{
 		proc->cycle_to_wait = vm->cpu.cur_cycle +
