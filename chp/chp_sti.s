f6894a41487a952c763101a86f2862b2db5728dc
tmanet <tmanet@student.42.fr>
2016-05-12 18:20:57 +0200

    maj champion, merde sur le +5 -1:

--- chp/chp_sti.s
+++ chp/chp_sti.s
@@ -8,9 +8,18 @@ first:		sti r1, %100, %1
 		sti r1, %-600, %-1
 		sti r1, %300, %-1
 		#sti r1, %600, %-1
-		sti r1, %344, %0
-		sti r1, -5, %0
+		sti r1, %344, %1
+		sti r1, %344, %2
+		sti r1, 5, %0
+		sti r1, 5, %1
+		sti r1, 5, %2
+		sti r1, 5, %3
+		sti r1, 5, %-1
+		sti r1, 5, %-2
 		sti r1, -5, %1
+		sti r1, -5, %1
+		sti r1, -5, %2
+		sti r1, 5, %2
 		sti r1, %258, %1
 		sti r1, 0, %0
 		sti r1, %0, %1
