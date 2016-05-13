71db5f5afe264f15ccf5bca196b507d611599995
tmanet <tmanet@student.42.fr>
2016-05-13 15:52:51 +0200

    wait

--- vm/sources/ft_wait.c
+++ vm/sources/ft_wait.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/13 15:33:39 by tmanet            #+#    #+#             */
-/*   Updated: 2016/05/13 15:43:11 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/05/13 15:52:11 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int	ft_wait(void)
 {
-	char	**ptr;
+	char	*ptr;
 
 	ptr = NULL;
-	if (get_next_line(0, ptr) < 0)
+	if (get_next_line(0, &ptr) < 0)
 		ft_error("fail to wait");
-	ft_memdel((void**)ptr);
+	ft_memdel((void**)&ptr);
 	return(1);
 }
