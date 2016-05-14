5753a93da44a82dfbe611e898f5cf17fa5405046
ninja00 <jolydavid.2112@yahoo.fr>
2016-05-14 05:30:04 -0600

    kill or not to kill proc, that is the question

--- vm/sources/ft_kill_proc.c
+++ vm/sources/ft_kill_proc.c
@@ -27,3 +27,21 @@ void	ft_kill_proc(t_vm *vm, t_process *proc)
 		ptr = &((*ptr)->next);
 	}
 }
+
+int	to_kill_or_not_to_kill_proc(t_vm *vm)
+{
+	t_process	ptr;
+	
+	ptr = vm->proc; 
+	while (ptr)
+	{
+		if (ptr->live < 1)
+			ft_kill_proc(vm, ptr);
+		else
+			ptr->live = 0;
+		ptr = ptr->next;
+	}
+	// QUOI SI TOUS LES PROC SONT MORT ?
+	//FIN 
+	return (0);
+}
