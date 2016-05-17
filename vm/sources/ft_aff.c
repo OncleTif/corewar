7403cedb93ce09a522807468b8c8a13d66600a82
tmanet <tmanet@student.42.fr>
2016-05-17 17:55:52 +0200

    ajout .h pour line ds aff

--- vm/sources/ft_aff.c
+++ vm/sources/ft_aff.c
@@ -6,7 +6,7 @@
 /*   By: eozdek <eozdek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/06 10:18:34 by tmanet            #+#    #+#             */
-/*   Updated: 2016/05/13 17:49:31 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/05/17 17:55:34 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 void	ft_aff(t_vm *vm, t_process *proc)
 {
+	int arg[3];
+
+	arg[0] = proc->ir.args[0];
 	if (vm->aff)
 		ft_putchar(proc->reg[proc->ir.args[0]] % 256);
+	if (vm->verbose & 4)
+		ft_print_operations(proc, arg);
 }
