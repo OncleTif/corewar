bfc80b2cb25414038e7ae8844f9214c46ede6129
tmanet <tmanet@student.42.fr>
2016-05-17 10:33:35 +0200

    correction des segfault dans les arguments

--- vm/sources/ft_manage.c
+++ vm/sources/ft_manage.c
@@ -6,7 +6,7 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/09 18:10:10 by eozdek            #+#    #+#             */
-/*   Updated: 2016/05/13 18:21:49 by djoly            ###   ########.fr       */
+/*   Updated: 2016/05/17 10:31:54 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 
 /*
-checker si cest un ".cor" return 1, sinon 0
-*/
+   checker si cest un ".cor" return 1, sinon 0
+   */
 
 int		ft_check_args(char *av)
 {
 	char *str;
 
-//	ft_putstr(av);
+	//	ft_putstr(av);
 	str = ft_strstr(av, ".cor");
 	if (!str)
 		return (0);
@@ -36,8 +36,8 @@ int		ft_check_args(char *av)
 }
 
 /*
-fonction check si le dump et num_plr est bien un nombre
-*/
+   fonction check si le dump et num_plr est bien un nombre
+   */
 
 int		ft_is_number(char *str)
 {
@@ -54,8 +54,8 @@ int		ft_is_number(char *str)
 }
 
 /*
-fonction recupere le dump et stock dans la T_vm
-*/
+   fonction recupere le dump et stock dans la T_vm
+   */
 
 
 int		ft_stock_aff(t_vm *vm)
@@ -67,7 +67,8 @@ int		ft_stock_aff(t_vm *vm)
 int		ft_stock_verb(char **av, int *i, t_vm *vm)
 {
 	(*i)++;
-	if (ft_is_number(av[*i]) == 0)
+	ft_putendl("av check");
+	if (!av[*i] || ft_is_number(av[*i]) == 0)
 		ft_error("ARG is not a number");
 	vm->verbose = vm->verbose | (ft_atoi(av[*i]) & 63);
 	return (1);
@@ -76,7 +77,7 @@ int		ft_stock_verb(char **av, int *i, t_vm *vm)
 int		ft_stock_step(char **av, int *i, t_vm *vm)
 {
 	(*i)++;
-	if (ft_is_number(av[*i]) == 0)
+	if (!av[*i] || ft_is_number(av[*i]) == 0)
 		ft_error("ARG is not a number");
 	vm->step = ft_atoi(av[*i]);
 	return (1);
@@ -85,27 +86,27 @@ int		ft_stock_step(char **av, int *i, t_vm *vm)
 int		ft_stock_dump(char **av, int *i, t_vm *vm)
 {
 	(*i)++;
-	if (ft_is_number(av[*i]) == 0)
+	if (!av[*i] || ft_is_number(av[*i]) == 0)
 		ft_error("ARG is not a number");
 	vm->dump = ft_atoi(av[*i]);
 	return (1);
 }
 /*
-fonction recupere le verbose et stock dans la T_vm
-*/
+   fonction recupere le verbose et stock dans la T_vm
+   */
 
 int		ft_stock_verbose(char **av, int *i, t_vm *vm)
 {
 	(*i)++;
-	if (ft_is_number(av[*i]) == 0)
+	if (!av[*i] || ft_is_number(av[*i]) == 0)
 		ft_error("ARG is not a number");
 	vm->verbose = ft_atoi(av[*i]);
 	return (1);
 }
 
 /*
-Change les autre num de plr en cas de doublon
-*/
+   Change les autre num de plr en cas de doublon
+   */
 
 void	ft_check_other_num(t_base_player bplr)
 {
