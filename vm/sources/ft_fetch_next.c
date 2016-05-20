5205c61ab721694afd788bf2da6380602414b529
tmanet <tmanet@student.42.fr>
2016-05-20 15:32:15 +0200

    ajout de ir.opcode sur lecture d'un opcode

--- vm/sources/ft_fetch_next.c
+++ vm/sources/ft_fetch_next.c
@@ -6,7 +6,7 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/06 16:20:06 by tmanet            #+#    #+#             */
-/*   Updated: 2016/05/20 14:19:53 by djoly            ###   ########.fr       */
+/*   Updated: 2016/05/20 15:31:37 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,6 @@ void	ft_fetch_next(t_vm *vm, t_process *proc)
 	{
 		proc->cycle_to_wait = vm->cpu.cur_cycle +
 			g_op_tab[vm->core[proc->pc] - 1].cost;
+		proc->ir.opcode = vm->core[proc->pc];
 	}
 }
