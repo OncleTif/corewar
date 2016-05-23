51de0bcf5cdc5ca53c372c0b8188b45498f8fcb4
Simon SICARD <ssicard@e2r9p2.42.fr>
2016-05-23 10:42:23 +0200

    envoi init norme

--- vm/sources/init.c
+++ vm/sources/init.c
@@ -6,45 +6,46 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/05 09:25:36 by ssicard           #+#    #+#             */
-/*   Updated: 2016/05/20 14:27:56 by djoly            ###   ########.fr       */
+/*   Updated: 2016/05/23 10:42:12 by ssicard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/corewar.h"
 
 /*
-   ajout au debut de t_proc
-   */
-t_process	*ft_add_link(t_process *beg, int plr, int pc, t_process *father)
+** ajout au debut de t_proc
+*/
+
+t_process			*ft_add_link(t_process *beg, int plr, int pc, t_process *fa)
 {
-	t_process *tmp;
+	t_process		*tmp;
 
 	if (!(tmp = (t_process*)ft_memalloc(sizeof(t_process))))
 		ft_error("malloc error in new cell creating");
 	if (tmp)
 	{
-		ft_memcpy(tmp->reg, father->reg, sizeof(father->reg));
+		ft_memcpy(tmp->reg, fa->reg, sizeof(fa->reg));
 		tmp->cycle_to_wait = -1;
 		tmp->num_plr = plr;
 		tmp->pc = pc;
-		tmp->carry = father->carry;
+		tmp->carry = fa->carry;
 		tmp->next = beg;
 		tmp->live = father->live;
-		tmp->lst_live = father->lst_live;
+		tmp->lst_live = fa->lst_live;
 	}
 	return (tmp);
 }
 
-void	ft_init_pc(int plr, int pc, t_vm *vm, t_process *proc)
+void				ft_init_pc(int plr, int pc, t_vm *vm, t_process *proc)
 {
 	vm->proc = ft_add_link(vm->proc, plr, pc, proc);
 	vm->proc->num = vm->nb_proc++;
 	ft_fetch_next(vm, vm->proc);
 }
 
-int		ft_init_lst_proc(t_vm *vm)
+int					ft_init_lst_proc(t_vm *vm)
 {
-	t_process	*tmp;
+	t_process		*tmp;
 	t_list_player	*lst_play;
 
 	lst_play = vm->bplr.lst_plyr;
@@ -63,10 +64,10 @@ int		ft_init_lst_proc(t_vm *vm)
 	return (0);
 }
 
-int		copy_plr(t_vm *vm, t_bin *plr, int i)
+int					copy_plr(t_vm *vm, t_bin *plr, int i)
 {
-	int		j;
-	int		size;
+	int				j;
+	int				size;
 
 	size = r4oi(plr->prog_size);
 	j = 0;
@@ -83,9 +84,9 @@ int		copy_plr(t_vm *vm, t_bin *plr, int i)
 	return (0);
 }
 
-int		ft_init_arena2(t_vm *vm)
+int					ft_init_arena2(t_vm *vm)
 {
-	t_list_player *tmp;
+	t_list_player	*tmp;
 
 	tmp = vm->bplr.lst_plyr;
 	if (vm->bplr.nb_plyr == 1)
@@ -111,7 +112,7 @@ int		ft_init_arena2(t_vm *vm)
 	return (0);
 }
 
-int		ft_init_arena(t_vm *vm)
+int					ft_init_arena(t_vm *vm)
 {
 	int				n_plyr;
 	int				pos_delta;
@@ -120,7 +121,6 @@ int		ft_init_arena(t_vm *vm)
 	n_plyr = 0;
 	pos_delta = MEM_SIZE / vm->bplr.nb_plyr;
 	l_plyr = vm->bplr.lst_plyr;
-
 	while (l_plyr)
 	{
 		copy_plr(vm, l_plyr->plr, n_plyr * pos_delta);
