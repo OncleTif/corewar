f7da9877fb338896250c23ea58f251341c7048c6
Your Name <you@example.com>
2016-05-24 18:55:30 +0200

    print finish ok

--- corewar_dir/sources/main.c
+++ corewar_dir/sources/main.c
@@ -6,7 +6,7 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 16:46:44 by djoly             #+#    #+#             */
-/*   Updated: 2016/05/20 16:44:46 by djoly            ###   ########.fr       */
+/*   Updated: 2016/05/24 18:28:02 by djoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -213,8 +213,9 @@ void		print_finish(t_vm *vm)
 		tmp = (-1 * win->num_plyr);
 	else
 		tmp = win->num_plyr;
-	ft_printf("Contestant %d (%s), has won !\n", tmp, win->prog_name); // merde si on a fait un -n
-	if (vm->verbose & 32 || vm->dump > 0)
+	if (vm->dump != vm->cpu.cur_cycle)
+		ft_printf("Contestant %d, \"%s\", has won !\n", tmp, win->prog_name); // merde si on a fait un -n
+	if (vm->dump == vm->cpu.cur_cycle)
 	{
 		//print_t_cpu(vm);
 		print_core(vm);
