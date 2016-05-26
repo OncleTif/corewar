c93601cf593e949dd6dc9cf94da9d6e98b54f55d
tmanet <tmanet@student.42.fr>
2016-05-26 15:47:04 +0200

    norme .h

--- corewar_dir/includes/player.h
+++ corewar_dir/includes/player.h
@@ -6,53 +6,42 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 13:31:38 by djoly             #+#    #+#             */
-/*   Updated: 2016/05/20 18:30:28 by djoly            ###   ########.fr       */
+/*   Updated: 2016/05/26 15:43:07 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PLAYER_H
 # define PLAYER_H
 # include "corewar.h"
-/*
-typedef struct s_player	t_player;
-struct s_player
-{
-	int		num_plyr;
-	char	name[PROG_NAME_LENGTH];
-	char	comment[COMMENT_LENGTH];
-	char	program[CHAMP_MAX_SIZE];
-};
-*/
-struct      s_bin
+
+struct		s_bin
 {
 	int					pc_tmp;
-	int					num_plyr; // la VM ou -n attribut un numero au joueur
+	int					num_plyr;
 	unsigned int		magic;
 	char				prog_name[PROG_NAME_LENGTH + 1];
 	unsigned int		prog_size;
 	char				comment[COMMENT_LENGTH + 1];
 	unsigned char		program[CHAMP_MAX_SIZE + 1];
-	int					last_live; // dernier cycle ou le plr a dit vivant
+	int					last_live;
 	int					nbr_live;
 	char				*name;
 	char				*com;
 	int					size;
 };
 
-struct s_list_player
+struct		s_list_player
 {
-
 	t_bin			*plr;
 	t_list_player	*next;
 };
 
-struct s_base_player
+struct		s_base_player
 {
-	int				tmpnum;	// stock le numero du plr qui a etait modifier
-	int				i_plr;//tmp
-	unsigned int 	tab[4]; // stock les num des champs par defaut
-								// peut etre modif par -n ***
-	int				nb_plyr; //nombre de player
+	int				tmpnum;
+	int				i_plr;
+	unsigned int	tab[4];
+	int				nb_plyr;
 	t_list_player	*lst_plyr;
 };
 
--- corewar_dir/includes/reverse_octet.h
+++ corewar_dir/includes/reverse_octet.h
@@ -6,7 +6,7 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 09:59:24 by djoly             #+#    #+#             */
-/*   Updated: 2016/05/01 10:53:24 by djoly            ###   ########.fr       */
+/*   Updated: 2016/05/26 15:46:21 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,24 @@
 # define REVERSE_OCTET_H
 # include "corewar.h"
 
-union	u_2o
+union			u_2o
 {
-	short int 	i;
+	short int	i;
 	char		c[2];
 };
 
-union	u_4o
+union			u_4o
 {
-	int 	i;
-	char	c[4];
+	int			i;
+	char		c[4];
 };
 
-short int	r2oi(short int val);
-int			r4oi(int val);
+short int		r2oi(short int val);
+int				r4oi(int val);
 
-// reverse octet avec arg et return en union*
-union u_2o *r2o(union u_2o *val); // reverse 2 octet
-union u_4o *r4o(union u_4o *val); // reverse 4 octet
-
-// reverse octet avec arg et return en char*
-char *r2oc(char *val);// reverse 2 octet
-char *r4oc(char *val); // reverse 4 octet
-
+union u_2o		*r2o(union u_2o *val);
+union u_4o		*r4o(union u_4o *val);
+char			*r2oc(char *val);
+char			*r4oc(char *val);
 
 #endif
